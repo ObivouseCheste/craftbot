8f1d59fe3114d6f3915d7c6a5f0b1d3a129c97f2
Amade <ynimos@gmail.com>
2014-07-10 22:44:07 -0500

    Created buttbot
    Added buttbot.py with basic functionality

---
+++ buttbot.py
@@ -0,0 +1,55 @@
+#from amde with butts
+
+import ircbot
+
+class ButtBot(IrcBot):
+	''' does the butts '''
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.dobutt = True
+		self.loopfuncs.extend([self.butt, self.tipbutt])
+
+	def makebutt(self, w):
+		if len(w) > 3:
+			if(w[-3:] == "ing"):
+				return "butting"
+			if(w[-2:] == "'s"):
+				return "butt's"
+			if(w[-1:] == "s"):
+				return "butts"
+		return "butt"
+
+	def dobuttmsg(self):
+		if self.dobutt:
+			self.dobutt = False
+			return True
+		if random.randrange(10) == 0:
+			return True
+		return False
+	
+	def butt(self):
+		msg = self.m['msg']
+		if self.dobuttmsg() == True:
+			newmsg = []
+			mybool = False
+			for w in msg.split():
+				if random.randrange(4) == 0:
+					w = self.makebutt(w)
+					mybool = True
+				newmsg.append(w)
+			if mybool == True:
+				self.say(' '.join(newmsg), self.m['target'])
+
+	def tipbutt(self):
+		if "thank you, buttbot" in self.m['msg'].lower() or "thank you buttbot" in self.m['msg'].lower():
+			self.say("\u0001ACTION tips butt\u0001", self.m['target'])
+		else:
+			if "buttbot" in self.m['msg'].lower():
+				self.dobutt = True
+
+if __name__ == "__main__":
+	class CmenBot(ButtBot):
+		''' Our beloved cmenbot <3 '''
+		pass
+	
+	CmenBot().loop()
